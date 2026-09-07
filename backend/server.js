12263eb9e58201e6b6537baad52fe769d18732f1
marcgosu-create <marcgosu@gmail.com>
2026-09-07 20:39:52 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import authRoutes from "./routes/auth.js";
+import elszamolasRoutes from "./routes/elszamolas.js";
+import szervizRoutes from "./routes/szerviz.js";
+import beosztasRoutes from "./routes/beosztas.js";
+import szabadsagRoutes from "./routes/szabadsag.js";
+import kifizetesRoutes from "./routes/kifizetes.js";
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/elszamolas", elszamolasRoutes);
+app.use("/szerviz", szervizRoutes);
+app.use("/beosztas", beosztasRoutes);
+app.use("/szabadsag", szabadsagRoutes);
+app.use("/kifizetes", kifizetesRoutes);
+app.use("/users", usersRoutes);
+
+app.listen(3000, () => {
+    console.log("HS Manager backend fut a 3000-es porton");
+});
